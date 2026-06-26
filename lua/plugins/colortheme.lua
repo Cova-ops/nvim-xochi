431ad2148b068c8ffdb9bb4d18d91a6f8f857a55
Daniel Covarrubias <54122783+Cova-ops@users.noreply.github.com>
2026-06-26 09:11:18 -0600

    change thema

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,27 +1,10 @@
--- return 	{
---     'folke/tokyonight.nvim',
---     priority = 1000,
---     config = function()
---         require('tokyonight').setup {
---             style = "storm",
---             transparent = true,
---             -- terminal_colors = true,
---             styles = {
---                 -- sidebars = "transparent",
---                 -- floats = "transparent",
---             },
---         }
---         vim.cmd.colorscheme 'tokyonight'
---     end,
--- }
-
 return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
 		opts = {
-			flavour = "mocha", -- opciones: latte, frappe, macchiato, mocha
+			flavour = "macchiato", -- opciones: latte, frappe, macchiato, mocha
 		},
 		config = function(_, opts)
 			require("catppuccin").setup(opts)
