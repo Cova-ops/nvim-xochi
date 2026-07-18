366ab6a693a82c2142116df4cae1dd702177af71
Daniel Covarrubias <54122783+Cova-ops@users.noreply.github.com>
2026-07-18 11:28:35 -0600

    more colorscheme options

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,10 +1,29 @@
 return {
+	-- {
+	-- 	"philosofonusus/morta.nvim",
+	-- 	name = "morta",
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme("morta")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"navarasu/onedark.nvim",
+	-- 	priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	config = function()
+	-- 		require("onedark").setup({
+	-- 			style = "darker",
+	-- 		})
+	-- 		require("onedark").load()
+	-- 	end,
+	-- },
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
 		priority = 1000,
 		opts = {
-			flavour = "macchiato", -- opciones: latte, frappe, macchiato, mocha
+			flavour = "macchiato", -- options: latte, frappe, macchiato, mocha
 		},
 		config = function(_, opts)
 			require("catppuccin").setup(opts)
