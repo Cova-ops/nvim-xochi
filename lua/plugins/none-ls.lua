269e959574845454f8abb84410104ece22dcc8ff
Daniel Covarrubias <54122783+Cova-ops@users.noreply.github.com>
2026-07-18 11:28:19 -0600

    modify format on save with :w and :W commands

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,7 +5,7 @@ return {
 		"jayp0521/mason-null-ls.nvim",
 	},
 	config = function()
-		local null_ls = require("null-ls") -- ✅ este es el fix
+		local null_ls = require("null-ls")
 		local formatting = null_ls.builtins.formatting
 		local diagnostics = null_ls.builtins.diagnostics
 
@@ -44,26 +44,15 @@ return {
 			require("none-ls.formatting.ruff_format"),
 		}
 
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		null_ls.setup({
-			sources = sources,
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({
-								async = false,
-								filter = function(c)
-									return c.name == "null-ls" -- ✅ nombre del client sigue siendo "null-ls"
-								end,
-							})
-						end,
-					})
-				end
-			end,
-		})
+		null_ls.setup({ sources = sources })
+
+		-- Save with format
+		vim.api.nvim_create_user_command("W", function()
+			vim.lsp.buf.format({
+				async = false,
+			})
+
+			vim.cmd("write")
+		end, {})
 	end,
 }
