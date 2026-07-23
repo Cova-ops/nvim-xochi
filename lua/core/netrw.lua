e8c76620fa9e84d40d81c841610aef5e4e7ffa38
Daniel Covarrubias <54122783+Cova-ops@users.noreply.github.com>
2026-07-23 13:27:43 -0600

    refact(netrw): update config

--- lua/core/netrw.lua
+++ lua/core/netrw.lua
@@ -3,5 +3,7 @@ vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 0
 vim.g.netrw_winsize = 25
 vim.g.netrw_keepdir = 0
-vim.g.netrw_altv = 1
 vim.g.netrw_preview = 1
+
+vim.g.netrw_list_hide = [[^\.\.\?$,^\..*]]
+vim.g.netrw_sort_sequence = [[[\/]$,*,\.bak$,\.o$,\.obj$,\.info$,\.swp$,\.tmp$]]
