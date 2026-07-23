17c45af8015f9252f155a26f861a12d985914686
Daniel Covarrubias <54122783+Cova-ops@users.noreply.github.com>
2026-07-23 13:38:02 -0600

    fix(netrw): correct apply config

--- lua/core/netrw.lua
+++ lua/core/netrw.lua
@@ -1,9 +1,8 @@
-vim.g.netrw_banner = 0
+vim.g.netrw_banner = 1
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 0
-vim.g.netrw_winsize = 25
+vim.g.netrw_winsize = 35
 vim.g.netrw_keepdir = 0
-vim.g.netrw_preview = 1
 
 vim.g.netrw_list_hide = [[^\.\.\?$,^\..*]]
 vim.g.netrw_sort_sequence = [[[\/]$,*,\.bak$,\.o$,\.obj$,\.info$,\.swp$,\.tmp$]]
