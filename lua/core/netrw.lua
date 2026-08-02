a276f00694ae73a152bfcf223dd53e38696ab465
Daniel Covarrubias <54122783+Cova-ops@users.noreply.github.com>
2026-08-02 08:21:00 -0600

    chore(netrw): Last modification

--- lua/core/netrw.lua
+++ lua/core/netrw.lua
@@ -2,7 +2,7 @@ vim.g.netrw_banner = 1
 vim.g.netrw_liststyle = 3
 vim.g.netrw_browse_split = 0
 vim.g.netrw_winsize = 35
-vim.g.netrw_keepdir = 0
+vim.g.netrw_keepdir = 1
 
-vim.g.netrw_list_hide = [[^\.\.\?$,^\..*]]
+vim.g.netrw_list_hide = [[^\.\.\?$]]
 vim.g.netrw_sort_sequence = [[[\/]$,*,\.bak$,\.o$,\.obj$,\.info$,\.swp$,\.tmp$]]
